2ec9c066c35b7319c0417df1166a6055fe4ca40c
Harneet Singh <harneet.singh.ts@gmail.com>
2020-02-09 14:18:47 +0530

    create notice functionality done

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ var express=require('express');
 var bodyparser=require('body-parser');
 var nodemailer=require('nodemailer');
 var config=require('./config');
+var multer = require('express-fileupload');
 
 var s_registerController=require('./student_register');
 var departmentController=require('./department');
 var courseController=require('./course');
 var loginController=require('./login');
 var activateController=require('./activate');
+var createNoticeController = require('./createNotice');
 
 const port = process.env.PORT || 3000;
 var app=express();
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}));
+app.use(multer());
+//app.use(bodyparser.json({limit: '30mb', extended: true}));
+//app.use(bodyparser.urlencoded({limit: '30mb', extended: true}));
 
 
 app.post('/student_register',(req,res)=>{
@@ -34,11 +39,14 @@ app.post('/login',(req,res)=>{
 });
 
 app.get('/activate',(req,res)=>{
-  console.log('hello');
   console.log(req.query.id);
   activateController.activateUser(req.query.id,res);
 });
 
+app.post('/create',(req,res)=>{
+  console.log("hit");
+  createNoticeController.createNotice(req,res);
+});
 
 app.listen(port,()=>{
   console.log(`Server is running at port `+port);
