60e5b43ee6914d424731c457c04fe82aea6c2084
LizaMNNIT <singlaliza98@gmail.com>
2020-02-15 16:51:04 +0530

    activate updated

--- activate.js
+++ activate.js
@@ -4,19 +4,31 @@ class activate{
 
 static activateUser(reg_id,res)
 {
-  connection.query('update student_registration set isActivated=1 where Reg_id=?',[reg_id],function(error,fields,results){
-   if(error){
-     res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write('<h1>Error</h1><br /><br />Internal Server Error');
-            res.end();
-   }
-   else {
-     res.writeHead(200, { 'Content-Type': 'text/html' });
-          res.write('<h1 style="color:green;text-align:center">Success</h1><br /><h2 style="color:blue;text-align:center;">Account activated, you can now login to continue<h2>');
-          res.end();
-   }
+  connection.query('select * from student_registration where Reg_id=?',[reg_id],function(error,fields,results){
+    if(fields[0].isActivated!=0)
+    {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+             res.write('<h1 style="color:green;text-align:center" > Account already activated</h1><br /><h2 style="color:blue;text-align:center;">You can directly login</h2>');
+             res.end();
+    }
+    else{
 
+      connection.query('update student_registration set isActivated=1 where Reg_id=?',[reg_id],function(error,fields,results){
+       if(error){
+         res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write('<h1 style="color:red;text-align:center" >Error</h1><br /><h2  style="color:blue;text-align:center;">Internal Server Error</h2>');
+                res.end();
+       }
+       else {
+         res.writeHead(200, { 'Content-Type': 'text/html' });
+              res.write('<h1 style="color:green;text-align:center">Success</h1><br /><h2 style="color:blue;text-align:center;">Account activated, you can now login to continue</h2>');
+              res.end();
+       }
+
+      });
+    }
   });
+
 }
 
 }
