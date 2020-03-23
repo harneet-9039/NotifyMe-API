f69e89db2fc94292e1c3e628c8a7db73d85ddd55
Harneet Singh <harneet.singh.ts@gmail.com>
2020-03-23 23:31:34 +0530

    update for change

--- student_register.js
+++ student_register.js
@@ -1,10 +1,21 @@
 var connection=require('./config');
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+  "19478322100-ci86q8k7ra4b2p960l6s2ktgts9860rb.apps.googleusercontent.com",
+  "0ngstkira0AfvwBq1_aDfGfE",
+  "https://developers.google.com/oauthplayground" // Redirect URL
+);
 
 class s_register{
 
     static s_registerUser(id,name,dept_id,course_id,year,email_id,contact,password,isCoordinator,nodemailer,req,res)
     {
-
+      oauth2Client.setCredentials({
+        refresh_token: "1//04vrMd0YzRvrVCgYIARAAGAQSNwF-L9IrjqWKvdgSq83vYaWiGgsETh34EwLx3LZjAzSG-OCWWmJh0R-RdazwQQLTB-9KRCegmtg"
+   });
+   const accessToken = oauth2Client.getAccessToken()
       var students={
         "Reg_id":id,
         "name":name,
@@ -30,18 +41,21 @@ class s_register{
             if (fields[1][0].Code=='101')
             res.json({
               status:false,
+              code:401,
               message:'User already registered'
             })
 
             else if(fields[1][0].Code=='102')
             res.json({
               status:false,
+              code:402,
               message:'Email_id already registered'
             })
 
             else if(fields[1][0].Code=='103')
             res.json({
               status:false,
+              code:403,
               message:'contact already registered'
             })
 
@@ -49,8 +63,10 @@ class s_register{
               var smtpTransport=nodemailer.createTransport({
                 service:"Gmail",
                 auth:{
-                  user:"notifyMe9039@gmail.com",
-                  pass:"Notify@123"
+                  clientId: "19478322100-ci86q8k7ra4b2p960l6s2ktgts9860rb.apps.googleusercontent.com",
+                  clientSecret: "0ngstkira0AfvwBq1_aDfGfE",
+                  refreshToken: "1//04vrMd0YzRvrVCgYIARAAGAQSNwF-L9IrjqWKvdgSq83vYaWiGgsETh34EwLx3LZjAzSG-OCWWmJh0R-RdazwQQLTB-9KRCegmtg",
+                  accessToken: accessToken
                 }
               });
 
@@ -71,6 +87,7 @@ class s_register{
                   console.log(error);
                   res.json({
                     status:false,
+                    code:404,
                     message:"Email Id provided is not correct"
                   })
                 }
@@ -80,12 +97,14 @@ class s_register{
                   {
                     res.json({
                       status:false,
+                      code:501,
                       message:error.sqlMessage
                     })
                   }
                   else{
                   res.json({
                     status:true,
+                    code:200,
                     message:'User registered successfully , Check your gmail to activate'
                   })
                 }
