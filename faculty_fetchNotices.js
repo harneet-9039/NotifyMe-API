5be28dc681c772b5aeed297ce793c09c08736a29
Harneet Singh <harneet.singh.ts@gmail.com>
2020-03-26 18:07:07 +0530

    update for change

--- faculty_fetchNotices.js
+++ faculty_fetchNotices.js
@@ -6,7 +6,7 @@ static facultyUser(req,res)
 { 
     
       var dept_id=req.body.dept_id;
-      var sql=' (select n.Notice_id,d.Dept_name as department, c.Course_branch as course, f.Name ,CAST(f.contact AS char) as contact, f.Faculty_id, n.validity,n.priority,n.title,n.description,n.date_time,f.email_id,f.designation ,GROUP_CONCAT(a.File_path) Attachments,GROUP_CONCAT(i.Image_path) Images from  faculty_registration f inner join department d on f.dept_id=d.Dept_id inner join courses c on d.Dept_id=c.Course_id inner join notices n on n.F_sender_id=f.Faculty_id left join attachments a on n.Notice_id=a.NoticeID left join images i on n.Notice_id=i.NoticeID inner join scope sc on n.Notice_id=sc.NoticeID  where (sc.scope = 1 or (sc.scope=2 and sc.Dept_id ='+connection.escape(dept_id)+') or (sc.scope=3 or scope=4 and  sc.Course_id in (';
+      var sql=' (select n.Notice_id,d.Dept_name as department, c.Course_branch as course, f.name ,CAST(f.contact AS char) as contact, f.Faculty_id, n.validity,n.priority,n.title,n.description,n.date_time,f.email_id,f.designation ,GROUP_CONCAT(a.File_path) Attachments,GROUP_CONCAT(i.Image_path) Images from  faculty_registration f inner join department d on f.dept_id=d.Dept_id inner join courses c on d.Dept_id=c.Course_id inner join notices n on n.F_sender_id=f.Faculty_id left join attachments a on n.Notice_id=a.NoticeID left join images i on n.Notice_id=i.NoticeID inner join scope sc on n.Notice_id=sc.NoticeID  where (sc.scope = 1 or (sc.scope=2 and sc.Dept_id ='+connection.escape(dept_id)+') or (sc.scope=3 or scope=4 and  sc.Course_id in (';
       var array = JSON.parse(req.body.course_id);
        if(array.length>1)
        {
