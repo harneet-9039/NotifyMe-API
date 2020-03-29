34d8354cf2d1dc55ca56793f7b94f11a655b627f
Harneet Singh <harneet.singh.ts@gmail.com>
2020-03-29 17:28:42 +0530

    Notification functionality added

--- login.js
+++ login.js
@@ -35,6 +35,7 @@ static loginUser(reg_id,pass,token,res)
          connection.query('select s.Reg_id,s.name,s.dept_id,d.Dept_name,s.course_id,c.Course_branch,s.year,s.email_id,cast(s.contact as CHAR) as contact from student_registration s inner join department d on s.dept_id=d.Dept_id inner join courses c on s.course_id=c.Course_id where Reg_id=? ',[reg_id],function(error,fields,results){
            if(!error)
            {
+             var temp=fields;
             connection.query('call fetchtoken(?,?,@result);select @result "Code"',[reg_id,token],function(error,fields,results){
               if(error){
                 res.json({
@@ -47,7 +48,7 @@ static loginUser(reg_id,pass,token,res)
                 if(fields[1][0].Code=='300'){
                   res.json({
                     status:true,
-                    data:fields,
+                    data:temp,
                     code:'100',
                     message:'Student successfully logged in'
                   })
@@ -58,7 +59,7 @@ static loginUser(reg_id,pass,token,res)
                     {
                       res.json({
                         status:true,
-                        data:fields,
+                        data:temp,
                         code:'100',
                         message:'Student successfully logged in'
                       })
@@ -92,7 +93,7 @@ static loginUser(reg_id,pass,token,res)
        connection.query('select s.Reg_id,s.name,s.dept_id,d.Dept_name,s.course_id,c.Course_branch,s.year,s.email_id,cast(s.contact as CHAR) as contact from student_registration s inner join department d on s.dept_id=d.Dept_id inner join courses c on s.course_id=c.Course_id where Reg_id=? ',[reg_id],function(error,fields,results){
          if(!error)
          {
-           
+           var temp=fields;
           connection.query('call fetchtoken(?,?,@result);select @result "Code"',[reg_id,token],function(error,fields,results){
             if(error){
               res.json({
@@ -105,7 +106,7 @@ static loginUser(reg_id,pass,token,res)
               if(fields[1][0].Code=='300'){
                 res.json({
                   status:true,
-                  data:fields,
+                  data:temp,
                   code:'200',
                   message:'Coordinator successfully logged in'
                 })
@@ -116,7 +117,7 @@ static loginUser(reg_id,pass,token,res)
                   {
                     res.json({
                       status:true,
-                      data:fields,
+                      data:temp,
                       code:'200',
                       message:'Coordinator successfully logged in'
                     })
@@ -147,7 +148,7 @@ static loginUser(reg_id,pass,token,res)
  connection.query('select f.Faculty_id,f.Name,f.email_id,cast(f.contact as CHAR) as contact,f.dept_id,d1.Dept_name,f.designation,GROUP_CONCAT(fc.course_id) as Course_id ,GROUP_CONCAT(fc.year) as year,GROUP_CONCAT(c.Course_branch) as course_name,GROUP_CONCAT(c.Dept_id) as Course_Dept_id, GROUP_CONCAT(d.Dept_name) as Course_Dept_name from faculty_registration f inner join department d1 on f.dept_id=d1.Dept_id left join faculty_courses fc on f.Faculty_id=fc.faculty_id inner join courses c on fc.course_id=c.Course_id inner join department d on c.Dept_id=d.Dept_id where f.Faculty_id=? group by faculty_id ',[reg_id],function(error,fields,results){
          if(!error)
          {
-          
+          var temp=fields;
           connection.query('call fetchtoken(?,?,@result);select @result "Code"',[reg_id,token],function(error,fields,results){
             if(error){
               res.json({
@@ -160,7 +161,7 @@ static loginUser(reg_id,pass,token,res)
               if(fields[1][0].Code=='300'){
                 res.json({
                   status:true,
-                  data:fields,
+                  data:temp,
                   code:'300',
                   message:'Faculty successfully logged in'
                 })
@@ -171,7 +172,7 @@ static loginUser(reg_id,pass,token,res)
                   {
                     res.json({
                       status:true,
-                      data:fields,
+                      data:temp,
                       code:'300',
                       message:'Faculty successfully logged in'
                     })
